448d6437672e71011844a059a5ef79e973382aa4
melvin <rafal.rychwalski420@gmail.com>
2021-01-29 08:51:47 +0100

    3

---
+++ kolokwium/kolokwium-master/kol_1/zadanie3/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/address.cpp" "/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/build/CMakeFiles/main.dir/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/address.cpp.o"
+  "/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/employee.cpp" "/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/build/CMakeFiles/main.dir/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/employee.cpp.o"
+  "/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/main.cpp" "/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/build/CMakeFiles/main.dir/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ kolokwium/kolokwium-master/kol_1/zadanie3/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/address.cpp.o"
+  "CMakeFiles/main.dir/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/employee.cpp.o"
+  "CMakeFiles/main.dir/home/melvin/srodowiska/kolokwium/kolokwium/kolokwium-master/kol_1/zadanie3/myProgram/main.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
